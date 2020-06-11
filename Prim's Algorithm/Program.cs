f37c33b3f2b2577c66e4d18bba4dcf91cac26341
Ducky3264 <david.love.wii@gmail.com>
2020-06-10 22:38:01 -0500

    Laptop 6/10/20

--- Prim's Algorithm/Program.cs
+++ Prim's Algorithm/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Numerics;
 
 namespace Prim_s_Algorithm
 {
@@ -19,16 +21,18 @@ namespace Prim_s_Algorithm
     {
         public char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
         public int CurrNum = 0;
-        public Dictionary<VectorXY, char> Nodes = new Dictionary<VectorXY, char>();
+        public Dictionary<char, VectorXY> Nodes = new Dictionary<char, VectorXY>();
     }
-    public class Program
+    public class VectorInfo
     {
-        static void Main(string[] args)
+        public Values V = new Values();
+        
+        public VectorInfo()
         {
             List<int> UsedLengthsX = new List<int>();
             List<int> UsedLengthsY = new List<int>();
             var rand = new Random();
-            Values V = new Values();
+            
             for (int i = 0; i <= 8; i++)
             {
                 var RandNX = rand.Next(1, 10);
@@ -43,15 +47,67 @@ namespace Prim_s_Algorithm
                 }
                 UsedLengthsX.Add(RandNX);
                 UsedLengthsY.Add(RandNY);
-                V.Nodes.Add(new VectorXY(RandNX, RandNY), V.alpha[V.CurrNum]);
+                V.Nodes.Add(V.alpha[V.CurrNum], new VectorXY(RandNX, RandNY));
                 V.CurrNum++;
             }
-            
+        }
+        public override string ToString()
+        {
             for (int i = 0; i < V.Nodes.Count; i++)
             {
                 var S1 = V.Nodes.ElementAt(i);
                 Console.WriteLine($"{S1}");
+                
             }
+            return "";
+        }
+       //Grab x value
+       //Grab y value
+       //Grab 2nd of each
+       //Log
+       //Compare
+        
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            VectorInfo VI = new VectorInfo();
+            VI.ToString();
+            Console.Write("Press enter to begin sorting...");
+            Console.ReadLine();
+            //List<VectorXY> LVXY = new List<VectorXY>();
+            VectorXY[] VXYarr = new VectorXY[VI.V.Nodes.Values.Count];
+            VI.V.Nodes.Values.CopyTo(VXYarr, 0);
+            var VXYL = VXYarr.AsEnumerable<VectorXY>().ToList();
+            for (int i = 1; i <= VXYL.Count; i++)
+            {
+                for (int i1 = 1; i1 <= VXYL.Count; i1++)
+                {
+                    var compVXY = new VectorXY(i, i1);
+                    if (VXYL.Contains(compVXY))
+                    {
+                        Console.Write("Y");
+                    } else
+                    {
+                        Console.Write("0");
+                    }
+                    Console.Write("  ");
+                    
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+            Random rand = new Random();
+            char start = VI.V.alpha[rand.Next(0, 26)];
+            VectorXY StartXY = VI.V.Nodes[start];
+            //Todo: Create a while function that checks to see if there is anything not visited. If not, do the math to make the next visit, add it to a list, display this on a pretty print, and repeat.
+            //List<int> LX = VI.V.Nodes.Values.Select((x) => x.x).ToList();
+            //List<int> LY = VI.V.Nodes.Values.Select((x) => x.y).ToList();
+            //Dictionary<int, int> NODEXY = new Dictionary<int, int>();
+            
+            Console.WriteLine("Finish");
+            
         }
     }
 }
